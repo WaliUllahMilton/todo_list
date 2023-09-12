d3504c6554aafe38451969261e6ec0d1b79bf289
waliUllahMilton <waliullahmiltoncse@gmail.com>
2023-09-13 04:22:11 +0600

    completed

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-let button = document.querySelector("button")
-let list = document.querySelector(".list")
-let input = document.querySelector(".input")
-let err = document.querySelector("p")
+let button = document.querySelector("button");
+let list = document.querySelector(".list");
+let input = document.querySelector(".input");
+let err = document.querySelector("p");
 
 
 
@@ -9,22 +9,35 @@ let err = document.querySelector("p")
 button.addEventListener("click", () => {
     if (input.value == "" || input.value == "undefined") {
         err.innerHTML = "please enter value";
-        input.value = ""
+        input.value = "";
     } else {
-        let li = document.createElement("li")
-        li.innerHTML = input.value
-        list.appendChild(li)
+        let li = document.createElement("li");
+        li.innerHTML = input.value;
+        list.appendChild(li);
         let span = document.createElement("span");
         span.innerHTML = "x";
         li.appendChild(span);
     }
     input.value = "";
+    saveData();
 })
 list.addEventListener("click", (e) => {
     if (e.target.tagName === "LI") {
-        console.log(e.target.tagName)
-        e.target.classList.toggle("checked")
+        e.target.classList.toggle("checked");
+
+        saveData();
     } else if (e.target.tagName === "SPAN") {
-        e.target.parentElement.remove()
+        e.target.parentElement.remove();
+        saveData();
     }
-})
+}, false);
+
+function saveData() {
+    localStorage.setItem("data", list.innerHTML);
+}
+
+function showTask() {
+    list.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
